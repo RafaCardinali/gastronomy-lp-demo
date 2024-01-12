1c1e49e9b0ce564b7f6405ab5dc32ef57541b577
Rafael Cardinali <rflcardinali@gmail.com>
2024-01-12 14:21:27 -0300

    [GAST-15] code review adjustments
    cleaning comments
    
    erasing empty spaces
    
    css file name change
    
    changing logo style from style.css to menu.css
    
    changes to better nomenclatures
    
    modularization of
    js content

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
 const rootStyle = getComputedStyle(document.documentElement);
-const secondColor = rootStyle.getPropertyValue('--second-color').trim();
-const hamburguer = document.querySelector('.hamburguer__icon');
-const menu = document.querySelector('.hamburguer__content');
+const secondaryColor = rootStyle.getPropertyValue('--secondary-color').trim();
+const hamburguerButton = document.querySelector('.hamburguer__icon');
+const hamburguerContent = document.querySelector('.hamburguer__content');
+const hamburguerItemDropdown = document.querySelectorAll('.hamburguer__content__main__item a');
 
-hamburguer.addEventListener('click', function () {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-        hamburguer.style.color = '';
+function handleHamburguerContentVisibility () {
+    hamburguerContent.style.display = hamburguerContent.style.display === 'block' ? 'none' : 'block';
+};
+
+function toggleHamburguerIconColor () {
+    hamburguerButton.style.color = hamburguerContent.style.display === 'block' ? secondaryColor : '';
+}
+
+function showHamburguerMenu() {
+    handleHamburguerContentVisibility();
+    toggleHamburguerIconColor();
+}
+
+hamburguerButton.addEventListener('click', showHamburguerMenu);
+
+function toggleSubmenuIcon (submenuDropdown, submenuIcon) {
+    if (submenuDropdown.style.display === 'block') {
+        if (submenuIcon) submenuIcon.textContent = 'remove';
     } else {
-        menu.style.display = 'block';
-        hamburguer.style.color = secondColor;
+        if (submenuIcon) submenuIcon.textContent = 'add';
     }
-});
+}
 
-const dropdowns = document.querySelectorAll('.hamburguer__content__main__item a');
-dropdowns.forEach(function(dropdown) {
-    dropdown.addEventListener('click', function(event) {
-        const parentLink = event.target.closest('.hamburguer__content__main__item a');
-        const submenu = parentLink.nextElementSibling;
-        const icon = parentLink.querySelector('.material-symbols-outlined');
-        if (submenu) {
-            if (submenu.style.display === 'block') {
-                submenu.style.display = 'none';
-                if (icon) icon.textContent = 'add';
-            } else {
-                submenu.style.display = 'block';
-                if (icon) icon.textContent = 'remove';
-            }
-        }
+function handleSubmenuDropdownVisibility(submenuDropdown) {
+    submenuDropdown.style.display = submenuDropdown.style.display === 'block' ? 'none' : 'block';
+}
 
-        event.preventDefault();
+function handleSubmenuActions(submenuDropdown, submenuIcon) {
+    if (submenuDropdown) {
+        handleSubmenuDropdownVisibility(submenuDropdown);
+        toggleSubmenuIcon(submenuDropdown, submenuIcon);
+    }
+}
+
+function handleSubmenuDropdownClick(event) {
+    const menuDropdownItem  = event.target.closest('.hamburguer__content__main__item a');
+    const submenuDropdown = menuDropdownItem.nextElementSibling;
+    const submenuIcon = menuDropdownItem.querySelector('.material-symbols-outlined');
+
+    handleSubmenuActions(submenuDropdown, submenuIcon);
+
+    event.preventDefault();
+}
+
+function setupDropdownListeners(dropdowns) {
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener('click', handleSubmenuDropdownClick);
     });
-});
+}
+
+setupDropdownListeners(hamburguerItemDropdown);
